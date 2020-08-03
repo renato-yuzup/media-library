c6e81a0bebb5d588a648baebf8c2e049ff48f382
Renato Yamassaki <renato@yuzu-project.com>
2020-08-03 01:28:51 -0300

    Set up webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 var path = require('path');
+var HtmlWebPackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -9,6 +10,16 @@ module.exports = {
   resolve: {
     extensions: [ '.js', '.jsx' ]
   },
+  devServer: {
+    contentBase: path.join(__dirname, 'app'),
+    port: 7000,
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      title: 'My Media Library',
+      template: './src/index.html',
+    }),
+  ],
   module: {
     rules: [
       {
@@ -16,8 +27,8 @@ module.exports = {
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
-        }
-      }
-    ]
+        },
+      },
+    ],
   },
 };
