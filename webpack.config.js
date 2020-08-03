4f6e201b3a5e6e764bc0bcf3d364e8ac3c1654ed
Renato Yamassaki <renato@yuzu-project.com>
2020-08-03 00:39:04 -0300

    Set up webpack

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+var path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'app.js',
+    path: path.resolve(__dirname, 'app'),
+  },
+  resolve: {
+    extensions: [ '.js', '.jsx' ]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        }
+      }
+    ]
+  },
+};
